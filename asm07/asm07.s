d0f000897e5c40d440272790685533b95f03a2e9
Antoine <antoineabonneau94@gmail.com>
2023-03-08 18:18:00 +0100

    fixed bug

--- asm07/asm07.s
+++ asm07/asm07.s
@@ -1,3 +1,5 @@
+section .data
+	result db 8 dup(' ')
 
 section .text
 	global _start
@@ -31,6 +33,35 @@ _start:
 	jmp .add_below
 
 .done_adding:
-	mov ebx, eax
+	mov ebx, 10
+	xor ecx, ecx
+
+.to_decimal:
+	xor edx, edx
+	div ebx
+	push edx 	
+	inc ecx 
+	cmp eax, 0
+	jne .to_decimal
+
+	xor edx, edx
+
+.to_string:
+	pop eax
+	dec ecx		 ; pop stack until last digit
+	add al, 0x30 ; print '4' not 4 so we need ascii number
+	mov byte [result + edx], al ; mov char to result string
+	inc edx
+	cmp ecx, 0
+	jne .to_string
+
+	mov byte [result + edx], 0x0a ; add line feed at the end
+
+	mov ebx, 0x01
+	mov ecx, result
+	mov eax, 0x04
+	int 0x80
+
+	mov ebx, 0
 	mov eax, 0x01
 	int 0x80
